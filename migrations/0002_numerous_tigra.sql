53abd8a4083cc174874e84b15858316ebc1d89e5
Mohamed <mabdel8@students.towson.edu>
2023-12-21 22:29:30 -0500

    next commit

---
+++ migrations/0002_numerous_tigra.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "files" DROP COLUMN IF EXISTS "workspace_owner";--> statement-breakpoint
+ALTER TABLE "files" DROP COLUMN IF EXISTS "logo";--> statement-breakpoint
+ALTER TABLE "folders" DROP COLUMN IF EXISTS "logo";
